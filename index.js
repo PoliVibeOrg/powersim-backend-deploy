e039ee35f1f7f3db15231f622abdd0509ea726fd
PoliVibeOrg <pandittrisha1604@gmail.com>
2025-07-05 20:27:18 +0530

    Fix: use node-fetch v2 for backend compatibility

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
+const fetch = require('node-fetch');
 const app = express();
 const port = 3000;
 
